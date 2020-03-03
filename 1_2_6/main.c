cad19f8e3552f35f71a800700968cdc81f30a9b6
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-03-03 17:13:36 +0100

    Add files via upload

---
+++ 1_2_6/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int licz1, licz2, licz3;
+    scanf("%d%d%d", &licz1, &licz2, &licz3);
+    printf("%d\n%d\n%d\n", licz1, licz2, licz3);
+    return 0;
+}
