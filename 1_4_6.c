61abb53b1f61178e08a4e398ab1123d92db6b2c6
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-03-04 11:02:30 +0100

    Add files via upload

---
+++ 1_4_6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Podaj liczbe calkowita n wieksza niz 2:\n");
+    scanf("%d", &n);
+    int wynik = 1;
+    if(n>2){
+        for(int i = 2; i <=n; i+=2){
+            wynik*=i;
+        }
+        printf("%d", wynik);
+    }
+    else{
+        printf("Podana liczba jest mniejsza niz 2.");
+    }
+    return 0;
+}
