97f1ab3bf8f8d3f74acebf44e8acad3676838b0d
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-03-10 23:11:23 +0100

    Update 1_4_6.c

--- 1_4_6.c
+++ 1_4_6.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-    int n;
+    int n, i;
     printf("Podaj liczbe calkowita n wieksza niz 2:\n");
     scanf("%d", &n);
     int wynik = 1;
     if(n>2){
-        for(int i = 2; i <=n; i+=2){
+        for(i = 2; i <=n; i+=2){
             wynik*=i;
         }
         printf("%d", wynik);
