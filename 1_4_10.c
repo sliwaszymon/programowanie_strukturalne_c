e37706212af26cf07234a1d0290abdc55d9caaf4
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-03-10 17:56:24 +0100

    Add files via upload

---
+++ 1_4_10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Podaj nieujemna liczbe n\n");
+	int n, wynik, i;
+	scanf("%d", &n);
+	if(n < 0){
+		printf("Podana liczba jest ujemna");
+	}
+	else{
+		for(i=1; i<=n; i++){
+			if(i*i <= n){
+				wynik = i;
+			}
+		}
+		printf("Pierwiastek z twojej liczby to %d", wynik);
+	}
+	return 0;
+}
