836c01a9eeb67a1be228be5c987df7528a37934f
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-03-10 23:10:24 +0100

    Update 1_4_2.c

--- 1_4_2.c
+++ 1_4_2.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    int n, m, pomoc;
+    int n, m, i, pomoc;
     printf("Podaj liczbe oraz ilosc jej pierwszych wielokrotnosci ktore chcesz wyswietlic:\n");
     scanf("%d%d", &n, &m);
-    for (int i=1;i<=m;i++){
+    for (i=1;i<=m;i++){
         printf("%d\n", n*i);
     }
     return 0;
