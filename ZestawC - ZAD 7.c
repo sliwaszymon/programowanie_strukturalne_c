02b3726bf9b07781b20a05964948dc7503c9ea7b
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-04-08 11:08:33 +0200

    przed commitem sortowalo min->max

--- ZestawC - ZAD 7.c
+++ ZestawC - ZAD 7.c
@@ -31,7 +31,7 @@ void wypisz(double tab[], int n)
 void wypiszodtylu(double tab[], int n)
 {
     int i;
-    for (i=n; i>0; i++)
+    for (i=n-1; i>=0; i--)
     {
         printf("%f ", tab[i]);
     }
